ad9c402d7c575674ae627865f85da919c8554d0a
Karuna Sehgal <kas4005@med.cornell.edu>
2021-07-28 08:01:38 -0400

    Ready for Heroku

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,5 @@
 class HangpersonGame
-
+  attr_accessor :word, :guesses, :wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -7,10 +7,43 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
   def initialize(word)
-    @word = word
+    @word, @guesses, @wrong_guesses = word, '', ''
   end
+
+  def guess(letter)
+    raise ArgumentError if !letter || letter.empty? || !letter.match(/[A-Za-z]/)
+
+    letter.downcase!
+
+    return false if @guesses.index(letter) || @wrong_guesses.index(letter)
+
+    if word.index(letter)
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+  end
+
+  def word_with_guesses
+    displayed = ''
+    @word.each_char do |letter|
+      if @guesses.index(letter)
+        displayed << letter
+      else
+        displayed << '-'
+      end
+    end
+    displayed
+  end
+
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length >= 7
+    return :win if !word_with_guesses.index('-')
+    :play
+  end
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
