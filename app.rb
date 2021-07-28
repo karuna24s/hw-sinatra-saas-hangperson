8ac3d5230e7e225020056afb614eacbf2224f20e
Karuna Sehgal <kas4005@med.cornell.edu>
2021-07-28 08:11:40 -0400

    Hangperson works

--- app.rb
+++ app.rb
@@ -6,25 +6,25 @@ class HangpersonApp < Sinatra::Base
 
   enable :sessions
   register Sinatra::Flash
-  
+
   before do
     @game = session[:game] || HangpersonGame.new('')
   end
-  
+
   after do
     session[:game] = @game
   end
-  
+
   # These two routes are good examples of Sinatra syntax
   # to help you with the rest of the assignment
   get '/' do
     redirect '/new'
   end
-  
+
   get '/new' do
     erb :new
   end
-  
+
   post '/create' do
     # NOTE: don't change next line - it's needed by autograder!
     word = params[:word] || HangpersonGame.get_random_word
@@ -33,16 +33,22 @@ class HangpersonApp < Sinatra::Base
     @game = HangpersonGame.new(word)
     redirect '/show'
   end
-  
+
   # Use existing methods in HangpersonGame to process a guess.
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      guess = @game.guess(letter)
+      flash[:message] = 'You have already used that letter.' if !guess
+    rescue ArgumentError
+      flash[:message] = 'Invalid guess.'
+    end
     redirect '/show'
   end
-  
+
   # Everytime a guess is made, we should eventually end up at this route.
   # Use existing methods in HangpersonGame to check if player has
   # won, lost, or neither, and take the appropriate action.
@@ -50,17 +56,26 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    status = @game.check_win_or_lose
+    redirect '/win' if status == :win
+    redirect '/lose' if status == :lose
     erb :show # You may change/remove this line
   end
-  
+
   get '/win' do
     ### YOUR CODE HERE ###
+    status = @game.check_win_or_lose
+    redirect '/show' if status == :play
+    redirect '/lose' if status == :lose
     erb :win # You may change/remove this line
   end
-  
+
   get '/lose' do
     ### YOUR CODE HERE ###
+    status = @game.check_win_or_lose
+    redirect '/show' if status == :play
+    redirect '/win' if status == :win
     erb :lose # You may change/remove this line
   end
-  
+
 end
